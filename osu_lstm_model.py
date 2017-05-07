0535750fa9c0a039c583f35ea29f655b1824d60b
MengweiDavidWu <mw3520@nyu.edu>
2017-05-06 22:45:36 -0400

    lstm—model

--- osu_lstm_model.py
+++ osu_lstm_model.py
@@ -14,37 +14,47 @@ from keras.layers import LSTM
 from keras.layers import Conv1D, MaxPooling2D
 
 # Input sequence
-X_test = np.loadtxt("D:\\tandon\\AI_for_game\\osu_project\\training_input.csv",delimiter=",",skiprows=0)
-X_test = X_test[:,0:3]
-Y_test = np.loadtxt("D:\\tandon\\AI_for_game\\osu_project\\training_output.csv",delimiter=",",skiprows=0)
-Y_test = Y_test[:,-1:]
+X_test = np.loadtxt("D:\\tandon\\AI_for_game\\osu_project\\normed_training_input.csv",delimiter=",",skiprows=0)
+
+Y_test = np.loadtxt("D:\\tandon\\AI_for_game\\osu_project\\normed_training_output.csv",delimiter=",",skiprows=0)
+
+
 # Preprocess Data:
 X_test = np.array(X_test, dtype=float) # Convert to NP array.
 
 # target = wholeSequence[0:2] 
 Y_test = np.array(Y_test, dtype=float)
-X_test = X_test.reshape((500, 240, 3))
-Y_test = Y_test.reshape((500, 240, 1))
+
+N = int(X_test.shape[0] / 120)
+
+
+X_test = X_test.reshape((N, 120, 15))
+Y_test = Y_test.reshape((N, 120, 3))
 
 #build lstm model
 model = Sequential()
-model.add(LSTM(32, input_shape=(240,3),activation='tanh', recurrent_activation='hard_sigmoid',
+model.add(LSTM(64, input_shape=(120,15),activation='tanh', recurrent_activation='relu',
                             use_bias=True, kernel_initializer='glorot_uniform',
                             recurrent_initializer='orthogonal', bias_initializer='zeros',
                             unit_forget_bias=True, 
                             return_sequences=True))
 
-model.add(LSTM(32,return_sequences=True))
+model.add(LSTM(64,return_sequences=True, activation='relu'))
 #model.add(LSTM(32,input_length = 24000, input_dim=3,return_sequences=True))
-model.add(Dense(32))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(3, activation='sigmoid'))
 #model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(X_test, Y_test, epochs=20, batch_size=16, verbose=1)
-X_target = X_test[0:100]
-predict = model.predict(X_target, batch_size = 32, verbose = 1)
+model.fit(X_test, Y_test, epochs=13, batch_size=64, verbose=1)
+model.save('D:\\tandon\\AI_for_game\\osu_project\\refined_my_model_second .h5')
+'''
+X_target = X_test[0:50]
+predict = model.predict(X_target, verbose = 1)
 print (predict)
-#np.savetxt('D:\\tandon\\AI_for_game\\osu_project\\training_result.csv', predict, delimiter = ',') 
+np.savetxt('D:\\tandon\\AI_for_game\\osu_project\\normed_training_result.csv', predict, delimiter = ',') 
+model.save('D:\\tandon\\AI_for_game\\osu_project\\my_model.h5')
+model2 = model.load('D:\\tandon\\AI_for_game\\osu_project\\my_model.h5')
+'''
 '''
 print("data",data)
 print("target",target)
